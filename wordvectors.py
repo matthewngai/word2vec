54816ce327a2d4d6a7636e03aa65dbb96d167918
MATTHEW NGAI <MTFNGAI@uwaterloo.ca>
2017-02-21 00:17:32 -0500

    part 2

--- wordvectors.py
+++ wordvectors.py
@@ -62,3 +62,38 @@ for review in unlabeled_train["review"]:
 print(len(sentences))
 print(sentences[0])
 print(sentences[1])
+
+# Import the built-in logging module and configure it so that Word2Vec 
+# creates nice output messages
+import logging
+logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s',\
+    level=logging.INFO)
+
+# Set values for various parameters
+num_features = 300    # Word vector dimensionality                      
+min_word_count = 40   # Minimum word count                        
+num_workers = 4       # Number of threads to run in parallel
+context = 10          # Context window size                                                                                    
+downsampling = 1e-3   # Downsample setting for frequent words
+
+# Initialize and train the model (this will take some time)
+from gensim.models import word2vec
+print("Training model...")
+model = word2vec.Word2Vec(sentences, workers=num_workers, \
+            size=num_features, min_count = min_word_count, \
+            window = context, sample = downsampling)
+
+# If you don't plan to train the model any further, calling 
+# init_sims will make the model much more memory-efficient.
+model.init_sims(replace=True)
+
+# It can be helpful to create a meaningful model name and 
+# save the model for later use. You can load it later using Word2Vec.load()
+model_name = "300features_40minwords_10context"
+model.save(model_name)
+print(model.doesnt_match("man woman child kitchen".split()))
+print(model.doesnt_match("france england germany berlin".split()))
+print(model.doesnt_match("paris berlin london austria".split()))
+print(model.most_similar("man"))
+print(model.most_similar("queen"))
+print(model.most_similar("awful"))
